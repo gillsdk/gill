98feac0e0304446811f72c601a850b65a2039786
guibibeau <g.bibeaulaviolette@gmail.com>
2025-08-25 11:01:30 -0600

    fix: update BalanceCard to use lamportsToSol

--- examples/react-app/src/routes/headless/components/BalanceCard.tsx
+++ examples/react-app/src/routes/headless/components/BalanceCard.tsx
@@ -1,7 +1,7 @@
 import { useBalance } from "gill-react";
 import { Card } from "../../../components/Card";
 import { useWallet } from "@hermis/solana-headless-react";
-import { lamportsToSol } from "gill";
+import { address, lamportsToSol } from "gill";
 
 interface BalanceCardProps {
   balance?: number | string;
@@ -11,15 +11,13 @@ interface BalanceCardProps {
 export function BalanceCard({ loading }: BalanceCardProps) {
   const { publicKey } = useWallet();
 
-  const { balance } = useBalance({ address: publicKey?.toBase58() });
+  const { balance } = useBalance({ address: address(publicKey?.toBase58() ?? "") });
 
   console.log(balance);
   return (
     <Card title="Account Balance">
       <div className="text-2xl font-bold">
-        {loading ? <span className="text-gray-400">Loading...</span> : <span>
-          {lamportsToSol(balance) || 0} SOL
-          </span>}
+        {loading ? <span className="text-gray-400">Loading...</span> : <span>{lamportsToSol(balance) || 0} SOL</span>}
       </div>
     </Card>
   );
