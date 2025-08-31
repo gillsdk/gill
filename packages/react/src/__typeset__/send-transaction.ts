6d07f3be7b00de68c7dd1da6fa49897a3827d613
JKrishnaD <jayak5063@gmail.com>
2025-08-31 11:46:49 +0530

    feat: implement useSendTransaction hook

---
+++ packages/react/src/__typeset__/send-transaction.ts
@@ -0,0 +1,37 @@
+import { useSendTransaction } from "../hooks/send-transaction";
+import { Base64EncodedWireTransaction, SendTransactionApi} from "gill";
+
+// [DESCRIBE] useSendTransaction
+{
+  const signature = null as unknown as Base64EncodedWireTransaction | string;
+  {
+    const transaction = useSendTransaction({ signature });
+    transaction.signature satisfies ReturnType<SendTransactionApi["sendTransaction"]>;
+    // @ts-expect-error - Should not allow no argument
+    useSendTransaction();
+
+    // @ts-expect-error - Should not allow empty argument object
+    useSendTransaction({});
+  }
+
+  {
+    // Should accept `config` input
+    const transaction = useSendTransaction({
+      config: {
+        encoding: "base64",
+        preflightCommitment: "confirmed",
+        skipPreflight: false,
+      },
+      signature,
+    });
+    transaction.signature satisfies ReturnType<SendTransactionApi["sendTransaction"]>;
+  }
+
+  {
+    const transaction = useSendTransaction({
+      signature: "5Pj5fCupXLUePYn18JkY8SrRaWFiUctuDTRwvUy2ML9yvkENLb1QMYbcBGcBXRrSVDjp7RjUwk9a3rLC6gpvtYpZ",
+    });
+
+    transaction.signature satisfies ReturnType<SendTransactionApi["sendTransaction"]>;
+  }
+}
---
+++ packages/react/src/hooks/send-transaction.ts
@@ -0,0 +1,57 @@
+"use client";
+
+import { GillUseRpcHook } from "./types";
+import { useSolanaClient } from "./client";
+import { GILL_HOOK_CLIENT_KEY } from "../const";
+import { useMutation } from "@tanstack/react-query";
+import { Base64EncodedWireTransaction, SendTransactionApi, Simplify } from "gill";
+
+// Define the configuration type for the RPC method
+type RpConfig = Simplify<Parameters<SendTransactionApi["sendTransaction"]>[1]>;
+
+// Define the response type for the hook
+type UseSendTransactionResponse = ReturnType<SendTransactionApi["sendTransaction"]>;
+
+type UseSendTransactionInput<TConfig extends RpConfig = RpConfig> = GillUseRpcHook<TConfig> & {
+  config?: TConfig;
+  /**
+   *  The signed transaction in wire format, as a base-64 encoded string.
+   */
+  signature: Base64EncodedWireTransaction | string;
+};
+
+/**
+ * Send a transaction using the Solana RPC method of
+ * [`sendTransaction`](https://solana.com/docs/rpc/http/sendtransaction)
+ *
+ * @returns An object from useMutation, including `data` (the transaction signature)
+ */
+export function useSendTransaction<TConfig extends RpConfig = RpConfig>({
+  signature,
+  config,
+}: UseSendTransactionInput<TConfig>) {
+  const { rpc } = useSolanaClient();
+
+  const { data, ...rest } = useMutation({
+    mutationFn: async () => {
+      // The RPC method expects the base-64 encoded transaction as the first argument.
+      const response = await rpc
+        .sendTransaction(signature as Base64EncodedWireTransaction, {
+          encoding: "base64",
+          preflightCommitment: "confirmed",
+          skipPreflight: false,
+          ...(config || {}),
+        })
+        .send();
+      // return the transaction signature as a base-58 encoded string
+      return response;
+    },
+    mutationKey: [GILL_HOOK_CLIENT_KEY, "sendTransaction"],
+    networkMode: "offlineFirst",
+  });
+
+  return {
+    ...rest,
+    signature: data as UseSendTransactionResponse,
+  };
+}
