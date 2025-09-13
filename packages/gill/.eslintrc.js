91067da6efbeee56aa1650bc905afd8b104d0c97
JKrishnaD <jayak5063@gmail.com>
2025-09-13 12:57:03 +0530

    lint fix

--- packages/gill/.eslintrc.js
+++ packages/gill/.eslintrc.js
@@ -1,25 +1,3 @@
-const { resolve } = require("node:path");
-const { rules } = require("../eslint-config");
-
 module.exports = {
-  extends: "../../config-eslint/index.js",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    project: resolve(__dirname, "tsconfig.lint.json"),
-  },
-  ignorePatterns: [
-    "node_modules/",
-    "dist/",
-    "**/*.js",
-    "**/*.mjs",
-    "**/*.cjs",
-    "**/.eslintrc.js",
-    "**/eslint.config.js",
-  ],
-  rules: {
-    ...rules,
-    "no-console": "off",
-    "no-debugger": "off",
-  },
+  extends: "@gillsdk/config-eslint",
 };
--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -1,11 +1,1 @@
-const { resolve } = require("node:path");
-
-module.exports = {
-  extends: [resolve(__dirname, "../../eslint-config/index.js")],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    project: resolve(__dirname, "tsconfig.lint.json"),
-  },
-  ignorePatterns: ["node_modules/", "dist/", "**/.eslintrc.js", "**/eslint.config.js"],
-};
+module.exports = require("../config-eslint/eslint.config.mjs").default;
