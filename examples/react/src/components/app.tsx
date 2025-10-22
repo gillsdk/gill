23f90c4b56d79ad6de716a12ae8d54bc7a4321d1
Tobey Codes <tobey@tobey.codes>
2025-10-22 12:34:35 -0600

    feat: add react example with use balance hook

---
+++ examples/react/src/components/app.tsx
@@ -0,0 +1,9 @@
+import { Balance } from "./balance";
+
+export function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-4 gap-4">
+      <Balance />
+    </div>
+  );
+}
