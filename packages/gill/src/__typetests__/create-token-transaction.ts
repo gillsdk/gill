9a4c96c563bcdc09d6bedce41d763655c0620c10
Data Salaryman <datasalaryman@protonmail.com>
2025-11-06 09:06:25 +0800

    test(packages/gill): remove tests for nonlifetime transactions

--- packages/gill/src/__typetests__/create-token-transaction.ts
+++ packages/gill/src/__typetests__/create-token-transaction.ts
@@ -26,16 +26,6 @@ async () => {
       metadata,
     })) satisfies BaseTransactionMessage<"legacy">;
 
-    const txNotSignable = (await buildCreateTokenTransaction({
-      feePayer: signer,
-      version: "legacy",
-      mint,
-      metadata,
-      // @ts-expect-error Should not have a Lifetime
-    })) satisfies TransactionMessageWithBlockhashLifetime;
-
-    signTransactionMessageWithSigners(txNotSignable);
-
     const txSignable = (await buildCreateTokenTransaction({
       feePayer: signer,
       version: "legacy",
@@ -56,16 +46,6 @@ async () => {
       mint,
       metadata,
     })) satisfies BaseTransactionMessage<0>;
-
-    const txNotSignable = (await buildCreateTokenTransaction({
-      feePayer: signer,
-      version: 0,
-      mint,
-      metadata,
-      // @ts-expect-error Should not have a Lifetime
-    })) satisfies TransactionMessageWithBlockhashLifetime;
-
-    signTransactionMessageWithSigners(txNotSignable);
 
     const txSignable = (await buildCreateTokenTransaction({
       feePayer: signer,
